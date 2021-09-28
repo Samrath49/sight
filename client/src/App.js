0e57cb71dba9a2dd4efad0bcc7754faa8aaae915
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-09-28 11:45:17 +0530

    Socket linking

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,39 @@ import { Typography, AppBar } from '@material-ui/core';
 import VideoPlayer from './components/VideoPlayer';
 import Options from './components/Options';
 import Notifications from './components/Notifications';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        borderRadius: 15,
+        margin: '30px 100px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '600px',
+        border: '2px solid black',
+
+        [theme.breakpoints.down('xs')]: {
+            width: '90%',
+        },
+    },
+    image: {
+        marginLeft: '15px',
+    },
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+    },
+}));
 
 const App = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <AppBar position="static" color="inherit">
+        <div className={classes.wrapper}>
+            <AppBar className={classes.appBar} position="static" color="inherit">
                 <Typography varient="" align="center">Sight</Typography>
             </AppBar>
             <VideoPlayer />
