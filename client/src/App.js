62717ff2247ef33f4601176f9e7a2112b56dba56
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-09-28 11:22:18 +0530

    Starting Component building

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Typography, AppBar } from '@material-ui/core';
 
 const App = () => {
     return (
         <div>
-            Video Chat App
+            <AppBar position="static" color="inherit">
+                <Typography varient="" align="center">Sight</Typography>
+            </AppBar>
+            {/* VideoPlayer */}
+            {/* Options -> Notification */}
         </div>
     )
 }
